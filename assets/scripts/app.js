45ab121924e30cd50f4deb2dbe3f1b119cb49971
doodoodoong <doo_doong@hanmail.net>
2024-01-27 01:45:23 +0900

    var

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,9 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+let errorMessage = "An error occured!";
+
+outputResult(currentResult, calculationDescription);
