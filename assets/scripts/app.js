227b58a82257ad292e47df0437caa1c689e910c2
doodoodoong <doo_doong@hanmail.net>
2024-02-07 23:19:52 +0900

    function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  const result = currentResult + userInput.value;
+  outputResult(currentResult, "");
 }
 
-const additionalResult = add(1, 2);
-
-currentResult = additionalResult;
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener("click", add);
